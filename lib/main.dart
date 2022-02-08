8af7bb817edb6aa4161ce800d3d195f9a5afe245
Christian Abrokwa <Cabrokwa11@gmail.com>
2022-02-08 22:16:33 +0000

    Final commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 // @dart=2.9
 
 import 'package:flutter/material.dart';
-import 'package:http/http.dart';
+import 'package:http/http.dart' as http;
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'dart:convert';
 
@@ -20,6 +20,31 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+
+  var temp;
+  var description;
+  var currently;
+  var humidity;
+  var windspeed;
+
+  Future getWeather() async{
+    http.Response response = await http.get(Uri.parse("https://api.openweathermap.org/data/2.5/weather?q=Ghana&units=metric&appid=INSERTAPIKEY"));
+    var results = jsonDecode(response.body);
+    setState(() {
+      this.temp = results["main"]["temp"];
+      this.description = results["weather"][0]["description"];
+      this.currently = results["weather"][0]["main"];
+      this.humidity = results["main"]["humidity"];
+      this.windspeed = results["wind"]["speed"];
+      });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    this.getWeather();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -36,7 +61,7 @@ class _HomeState extends State<Home> {
             Padding(
               padding: EdgeInsets.only(bottom: 10.0 ),
               child: Text(
-                "Currently in Boston",
+                "Currently in Ghana",
                 style: TextStyle(
                   color: Colors.white,
                   fontSize: 14.0,
@@ -45,7 +70,7 @@ class _HomeState extends State<Home> {
                 ),
               ),
             Text(
-            "52\u00B0",
+              temp != null ? temp.toString() + "\u00B0" : "loading",
             style: TextStyle(
               color: Colors.white,
               fontSize: 40.0,
@@ -55,7 +80,7 @@ class _HomeState extends State<Home> {
             Padding(
               padding: EdgeInsets.only(top:  10.0 ),
               child: Text(
-                "Rain",
+                 currently != null ? currently.toString() : "loading",
                 style: TextStyle(
                   color: Colors.white,
                   fontSize: 14.0,
@@ -74,22 +99,22 @@ class _HomeState extends State<Home> {
                 ListTile(
                   leading: FaIcon(FontAwesomeIcons.thermometerHalf),
                   title: Text("Temperature"),
-                  trailing: Text("52\u00B0"),
+                  trailing: Text(temp != null ? temp.toString() + "\u00B0" : "loading"),
                    ),
                 ListTile(
                   leading: FaIcon(FontAwesomeIcons.cloud),
                   title: Text("Weather"),
-                  trailing: Text("Weather"),
+                  trailing: Text(description != null ? description.toString() : "loading"),
                    ),
                 ListTile(
                   leading: FaIcon(FontAwesomeIcons.sun),
                   title: Text("Humidity"),
-                  trailing: Text("12"),
+                  trailing: Text(humidity != null ? humidity.toString() : "loading"),
                    ),
                 ListTile(
                   leading: FaIcon(FontAwesomeIcons.wind),
                   title: Text("Wind Speed"),
-                  trailing: Text("12"),
+                  trailing: Text(windspeed != null ? windspeed.toString(): "loading"),
                    )
                ],
               ),
